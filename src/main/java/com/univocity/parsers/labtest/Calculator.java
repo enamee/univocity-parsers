420cfe034eb2ebc24c206fefff96d5b2f3d4d8e0
Enam E Elahi <appreel@LAPTOP-I439IDMG.localdomain>
2025-09-21 13:11:11 +0600

    Change divide error message in main

--- src/main/java/com/univocity/parsers/labtest/Calculator.java
+++ src/main/java/com/univocity/parsers/labtest/Calculator.java
@@ -12,8 +12,6 @@ public class Calculator {
     }
     public double divide(double a, double b) {
         if  (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
+            throw new IllegalArgumentException("Division by zero is not allowed");
         }
         return a / b;
-    }
-}
